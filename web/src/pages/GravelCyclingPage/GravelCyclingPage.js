c5a3e91947383481d1de1e1bb2d2532b15afd40f
Isaac Tait <isaac@mountaintopcoding.com>
2020-03-18 20:21:25 -0700

    back on the road again!

--- web/src/pages/GravelCyclingPage/GravelCyclingPage.js
+++ web/src/pages/GravelCyclingPage/GravelCyclingPage.js
@@ -1,11 +1,16 @@
 import { Link, routes } from '@redwoodjs/router'
 import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const GravelCyclingPage = () => {
   return (
     <BlogLayout>
+
       <h3>A Gravel Cycling Blog</h3>
           <p>Built using the venerable (and opinionated) package manager <a href="https://redwoodjs.com/">Redwood</a></p>
+
+          <BlogPostsCell />
+
           <nav>
             <ol>
               <li>
@@ -13,6 +18,7 @@ const GravelCyclingPage = () => {
               </li>
             </ol>
           </nav>
+
     </BlogLayout>
   )
 };
